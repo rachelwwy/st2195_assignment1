849e6a654632a009aa4d73c98730a71b58f9d00c
rachelwwy <rachelwwy86@gmail.com>
2024-02-11 22:59:13 +0400

    add jupyter

---
+++ Jupyter 1 to 10.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Writing Python code to show integers 1 to 10
+numbers = range(1,11)
+list (numbers)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
